f9c067145ed18c68f3a5c4e59467183b9212a052
KennethKcai <97841755+KennethKcai@users.noreply.github.com>
2024-04-16 00:20:35 -0700

    AD_80 range

--- api test/test.py
+++ api test/test.py
@@ -13,9 +13,23 @@ base_values = [item['base'] for item in json_data if 'base' in item]
 rounded_data_y = [[round(num, 1) for num in sublist] for sublist in y_values]
 rounded_data_base = [[round(num, 1) for num in sublist] for sublist in base_values]
 
-print(rounded_data_y[0])
-print(rounded_data_base[0])
+# print(rounded_data_y[0])
+# print(rounded_data_base[0])
 
+AD_80_y = rounded_data_y[0]
+AD_80_base = rounded_data_base[0]
+
+lst_range_80_min = []
+lst_range_80_max = []
+
+for i in range(len(AD_80_base)):
+    range_80_min = AD_80_base[i]
+    lst_range_80_min.append(range_80_min)
+    range_80_max = AD_80_base[i] + AD_80_y[i]
+    lst_range_80_max.append(range_80_max)
+
+print(lst_range_80_min)
+print(lst_range_80_max)
 
 # url = "https://api.zerowidth.ai/beta/process/XmZlDB2W1HFIzS7fmawI/fI8ys5r4pBiTnLdlQJdS"
 # headers = {
